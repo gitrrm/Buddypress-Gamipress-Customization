6e1ae7aeebbcc5e8995787289a50846be62a2102
gitrrm <63955302+gitrrm@users.noreply.github.com>
2023-10-26 00:07:06 +0530

    2nd Changes Commit
    Task 2,3,5,7

--- header.php
+++ header.php
@@ -19,8 +19,7 @@
         }
     ?>
    <div id="page" class="site">
-       <header id="masthead" class="site-header" role="banner">
-           <?php get_template_part('template-parts/header/nav'); ?>
-       </header>
+       <?php get_template_part('template-parts/header/nav'); ?>
        <div id="content" class="site-content">
+        
            
