beec3e0a45bd04cd9670350e3c5176067708681f
gitrrm <63955302+gitrrm@users.noreply.github.com>
2023-10-26 00:11:54 +0530

    2nd changes commit
    task 2,3,5,7

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -5,48 +5,44 @@
  * @package Nandan Global
  */
 ?>
-<header id="masthead" class="site-header">
-	<div class="site-branding">
-		<?php
-		the_custom_logo();
-		if ( is_front_page() && is_home() ) :
-			?>
+<header id="masthead" class="site-header container bg-white overflow-hidden pb-3 pt-3" role="banner"> 
+	<div class="site-branding float-start inline-block border-box ">
+		<?php the_custom_logo(); ?>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php
-		else :
-			?>
-			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-		endif;
-		$nandanglobal_description = get_bloginfo( 'description', 'display' );
-		if ( $nandanglobal_description || is_customize_preview() ) :
-			?>
-			<p class="site-description"><?php echo $nandanglobal_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+		<?php
+			$nandanglobal_description = get_bloginfo( 'description', 'display' );
+			if ( $nandanglobal_description || is_customize_preview() ) :
+		?>
+			<p class="site-description"><?php echo $nandanglobal_description; ?></p>
 		<?php endif; ?>
 	</div><!-- .site-branding -->
 
-	<nav id="site-navigation" class="main-navigation">
-		<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'nandanglobal' ); ?></button>
+	<nav id="site-navigation" class="main-navigation float-start inline-block border-box">
+		<button class="menu-toggle ms-5" aria-controls="primary-menu" aria-expanded="false">
+			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+			  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+			</svg>
+		</button>
 		<?php
 		wp_nav_menu(
 			array(
 				'theme_location' => 'menu-1',
 				'menu_id'        => 'primary-menu',
+				'menu_class'           => 'menu nav-justified'
 			)
 		);
 		?>
 	</nav><!-- #site-navigation -->
-	<div id="username">
+	<div id="username" class="float-end inline-block border-box">
 		<?php 
 		    $current_user = wp_get_current_user();
 		    $user_id = $current_user->ID;
 			
 			if (is_user_logged_in()) {
 
-			    // Get the user's avatar with a specific size (e.g., 64) and custom CSS class (optional).
 			    $avatar_image = get_avatar($user_id, 32, '', 'avatar-class');
 
-			    echo esc_html($current_user->user_firstname) . '  ' . $avatar_image;
+			    echo '<a href="#">'.esc_html($current_user->user_firstname) . '  ' . $avatar_image . '</a>';
 			} 
 		?>
 	</div>
