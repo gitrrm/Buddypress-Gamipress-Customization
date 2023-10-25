a3729b3468a9e2b4ec0c69e374cf9544a7e6b886
gitrrm <63955302+gitrrm@users.noreply.github.com>
2023-10-26 00:08:41 +0530

    2nd changes commit
    task 2,3,5,7

--- footer.php
+++ footer.php
@@ -5,10 +5,24 @@
  * @package Nandan.Global
  */ 
 ?>
+   <footer id="colophon" class="site-footer">
+      <div class="site-info bg-warning pt-2 pb-2 text-center">
+         <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nandanglobal' ) ); ?>" class="link-secondary" target="_blank">
+            <?php
+            /* translators: %s: CMS name, i.e. WordPress. */
+            printf( esc_html__( 'Proudly powered by %s', 'nandanglobal' ), 'WordPress' );
+            ?>
+         </a>
+         <span class="sep text-secondary"> | </span>
+            <?php
+            /* translators: 1: Theme name, 2: Theme author. */
+            printf( esc_html__( 'Theme: %1$s by %2$s.', 'nandanglobal' ), 'nandanglobal', '<a href="https://nandan.global/" class="link-secondary" target="_blank">Rashmi Ranjan Muduli</a>' );
+            ?>
+      </div><!-- .site-info -->
+   </footer><!-- #colophon -->
 
-
-</div>
-   </div>
+</div><!-- #content -->
+   </div><!-- #page -->
    <?php wp_footer(); ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,33 +8,26 @@
 get_header();
 
 ?>
-
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-
-            <?php
+<div class="container bg-light pb-5">
+    <div class="row justify-content-center">
+        <?php
             if (have_posts()) :
+            	if ( is_home() && ! is_front_page() ) :
+				?>
+					<header>
+						<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+					</header>
+					<?php
+				endif;
                 while (have_posts()) :
                     the_post();
-                    ?>
-                    <div class="card mb-3">
-                        <div class="card-body">
-                            <h2 class="card-title"><?php the_title(); ?></h2>
-                            <p class="card-text"><?php the_excerpt(); ?></p>
-                            <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read more</a>
-                        </div>
-                    </div>
-                    <?php
+                    get_template_part( 'template-parts/content', get_post_type() );             
                 endwhile;
+                the_posts_navigation();
             else :
                 echo 'No posts found.';
             endif;
-            ?>
-
-        </div><!-- .col-md-8 -->
-
+        ?>
         <?php get_sidebar(); // Include the sidebar template ?>
     </div><!-- .row -->
 </div><!-- .container -->
