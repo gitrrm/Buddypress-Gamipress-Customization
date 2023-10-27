324dc86818e02420ffdb8fb837ed5592d2a70304
gitrrm <63955302+gitrrm@users.noreply.github.com>
2023-10-27 08:42:27 +0530

    3rd Commit - page template updated

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -17,8 +17,8 @@
 		<?php endif; ?>
 	</div><!-- .site-branding -->
 
-	<nav id="site-navigation" class="main-navigation float-start inline-block border-box">
-		<button class="menu-toggle ms-5" aria-controls="primary-menu" aria-expanded="false">
+	<nav id="site-navigation" class="main-navigation float-start float-sm-end inline-block border-box">
+		<button class="menu-toggle ms-5 me-sm-0" aria-controls="primary-menu" aria-expanded="false">
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
 			  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
 			</svg>
